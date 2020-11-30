a913cbee5fef8392156172b27d6a262083ae9df4
frankmendez <frankmendezwebdev@gmail.com>
2020-11-30 20:33:35 +0800

    Add Name and Avatar to Users, Add New Users to Database

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import firebase from '../../firebase'
 import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import md5 from 'md5'
 
 export default class Register extends Component {
 	state = {
@@ -11,6 +12,7 @@ export default class Register extends Component {
 		passwordConfirmation: '',
 		errors: [],
 		loading: false,
+		usersRef: firebase.database().ref('users'),
 	}
 
 	displayErrors = (errors) => errors.map((error, i) => <p key={i}>{error.message}</p>)
@@ -60,13 +62,34 @@ export default class Register extends Component {
 				.createUserWithEmailAndPassword(this.state.email, this.state.password)
 				.then((createdUser) => {
 					console.log(createdUser)
-					this.setState({ loading: false })
+					createdUser.user
+						.updateProfile({
+							displayName: this.state.username,
+							photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`,
+						})
+						.then(() => {
+							//this.setState({ loading: false })
+							this.saveUser(createdUser).then(() => {
+								console.log('user saved')
+							})
+						})
+						.catch((err) => {
+							console.error(err)
+							this.setState({ errors: this.state.errors.concat(err), loading: false })
+						})
 				})
 				.catch((err) => {
 					console.error(err)
 					this.setState({ errors: this.state.errors.concat(err), loading: false })
 				})
 		}
+	}
+
+	saveUser = (createdUser) => {
+		return this.state.usersRef.child(createdUser.user.uid).set({
+			name: createdUser.user.displayName,
+			avatar: createdUser.user.photoURL,
+		})
 	}
 
 	handleInputError = (errors, input) => {
