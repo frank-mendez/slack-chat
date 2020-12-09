85279a31ed61f1fa005306ba0dc5dcb2cd35d07d
frankmendez <frankmendezwebdev@gmail.com>
2020-12-09 20:28:28 +0800

    Add Channels Component

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react'
+import Channels from './Channels'
 import UserPanel from './UserPanel'
 
 export class SidePanel extends Component {
@@ -8,6 +9,7 @@ export class SidePanel extends Component {
 		return (
 			<Menu size='large' inverted fixed='left' vertical style={{ background: '#4c3c4c', fontSize: '1.2rem' }}>
 				<UserPanel currentUser={currentUser} />
+				<Channels />
 			</Menu>
 		)
 	}
